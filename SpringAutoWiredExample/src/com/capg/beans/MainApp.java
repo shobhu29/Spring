cd9a8ff46b02e29b865875d821a6aad4bd0a9b70
Shobhit Mishra <46920235+shobhu29@users.noreply.github.com>
2019-02-28 16:37:55 +0530

    Add files via upload

---
+++ SpringAutoWiredExample/src/com/capg/beans/MainApp.java
@@ -0,0 +1,17 @@
+package com.capg.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee emp = (Employee)context.getBean("emp");
+		System.out.println("Employee ID is : " + emp.getEmp_id());
+		System.out.println("Employee Name is : " + emp.getEmp_name());
+		System.out.println("Employee City is : " +  emp.getAddress().getCity());
+		
+	}	
+}
